b55291a510ede4355d147920b72f58742a14603a
Andrew Roslik <andkirby@gmail.com>
2019-02-06 16:08:46 +0200

    Added 9000 port.

--- bin/include.sh
+++ bin/include.sh
@@ -19,10 +19,14 @@ vagrant_forward_type='port'
 
 export M2SETUP_PROJECT=${M2SETUP_PROJECT:-$(echo ${__dir} | sed -r 's|.*/([^/]+)/bin$|\1|')}
 export M2SETUP_VIRTUAL_HOST=${M2SETUP_PROJECT}.cc
+
+# Generate environment variable for ports
+# for "Host > VM > Docker" communications within different projects
 export M2SETUP_PORT_22=$(host_port ${M2SETUP_VIRTUAL_HOST} 22 ${vagrant_forward_type})
 export M2SETUP_PORT_80=$(host_port ${M2SETUP_VIRTUAL_HOST} 80 ${vagrant_forward_type})
 export M2SETUP_PORT_443=$(host_port ${M2SETUP_VIRTUAL_HOST} 443 ${vagrant_forward_type})
 export M2SETUP_PORT_3306=$(host_port ${M2SETUP_VIRTUAL_HOST} 3306 ${vagrant_forward_type})
+export M2SETUP_PORT_9000=$(host_port ${M2SETUP_VIRTUAL_HOST} 9000 ${vagrant_forward_type})
 
 dev_file_option=''
 if [ -f ${__dir}/../docker-compose.dev.yml ]; then
