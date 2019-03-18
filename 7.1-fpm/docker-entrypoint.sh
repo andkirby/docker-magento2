70c50b7c867b2fab336ffc775791534d4f873761
Andrew Roslik <andkirby@gmail.com>
2019-03-18 14:53:11 +0200

    Adopted code from php 7.2.

--- 7.1-fpm/docker-entrypoint.sh
+++ 7.1-fpm/docker-entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-[ "$DEBUG" = "true" ] && set -x
+set -o pipefail
+set -o errexit
+set -o nounset
+#set -o xtrace
+
+[ "${DEBUG:-}" = "true" ] && set -x
 
 # If asked, we'll ensure that the www-data is set to the same uid/gid as the
 # mounted volume.  This works around permission issues with virtualbox shared
 # folders.
-if [[ "$UPDATE_UID_GID" = "true" ]]; then
+if [[ "${UPDATE_UID_GID:-}" = "true" ]]; then
     echo "Updating www-data uid and gid"
 
     DOCKER_UID=`stat -c "%u" $MAGENTO_ROOT`
@@ -20,10 +25,10 @@ if [[ "$UPDATE_UID_GID" = "true" ]]; then
     # Once we've established the ids and incumbent ids then we need to free them
     # up (if necessary) and then make the change to www-data.
 
-    [ ! -z "${INCUMBENT_USER}" ] && usermod -u 99$DOCKER_UID $INCUMBENT_USER
+    [ -n "${INCUMBENT_USER}" ] && usermod -u 99$DOCKER_UID $INCUMBENT_USER
     usermod -u $DOCKER_UID www-data
 
-    [ ! -z "${INCUMBENT_GROUP}" ] && groupmod -g 99$DOCKER_GID $INCUMBENT_GROUP
+    [ -n "${INCUMBENT_GROUP}" ] && groupmod -g 99$DOCKER_GID $INCUMBENT_GROUP
     groupmod -g $DOCKER_GID www-data
 fi
 
@@ -31,25 +36,52 @@ fi
 mkdir -p $MAGENTO_ROOT
 chown www-data:www-data $MAGENTO_ROOT
 
+CRON_LOG=/var/log/cron.log
 
+# Setup Magento cron
+echo "* * * * * www-data /usr/local/bin/php ${MAGENTO_ROOT}/bin/magento cron:run | grep -v \"Ran jobs by schedule\" >> ${MAGENTO_ROOT}/var/log/magento.cron.log" > /etc/cron.d/magento
+echo "* * * * * www-data /usr/local/bin/php ${MAGENTO_ROOT}/update/cron.php >> ${MAGENTO_ROOT}/var/log/update.cron.log" >> /etc/cron.d/magento
+echo "* * * * * www-data /usr/local/bin/php ${MAGENTO_ROOT}/bin/magento setup:cron:run >> ${MAGENTO_ROOT}/var/log/setup.cron.log" >> /etc/cron.d/magento
+
+# Get rsyslog running for cron output
+touch $CRON_LOG
+echo "cron.* $CRON_LOG" > /etc/rsyslog.d/cron.conf
+service rsyslog start
 
 # Configure Sendmail if required
-if [ "$ENABLE_SENDMAIL" == "true" ]; then
+if [ "${ENABLE_SENDMAIL:-}" == "true" ]; then
     /etc/init.d/sendmail start
 fi
 
 # Substitute in php.ini values
-[ ! -z "${PHP_MEMORY_LIMIT}" ] && sed -i "s/!PHP_MEMORY_LIMIT!/${PHP_MEMORY_LIMIT}/" /usr/local/etc/php/conf.d/zz-magento.ini
-[ ! -z "${UPLOAD_MAX_FILESIZE}" ] && sed -i "s/!UPLOAD_MAX_FILESIZE!/${UPLOAD_MAX_FILESIZE}/" /usr/local/etc/php/conf.d/zz-magento.ini
+[ -n "${PHP_MEMORY_LIMIT:-}" ] \
+  && sed -i "s/memory_limit ?=.*/memory_limit = ${PHP_MEMORY_LIMIT}/" \
+        /usr/local/etc/php/conf.d/zz-magento.ini
 
-[ "$PHP_ENABLE_XDEBUG" = "true" ] && \
-    docker-php-ext-enable xdebug && \
-    echo "Xdebug is enabled"
+[ -n "${PHP_UPLOAD_MAX_FILESIZE:-}" ] \
+  && sed -i "s/upload_max_filesize ?=.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
+        /usr/local/etc/php/conf.d/zz-magento.ini
 
+# Enable Xdebug
+[ "${PHP_ENABLE_XDEBUG:-}" = "true" ] && xd_swi on || xd_swi off
 
-# Configure PHP-FPM
-[ ! -z "${MAGENTO_RUN_MODE}" ] && sed -i "s/!MAGENTO_RUN_MODE!/${MAGENTO_RUN_MODE}/" /usr/local/etc/php-fpm.conf
+# Configure composer
+[ -n "${COMPOSER_GITHUB_TOKEN:-}" ] && \
+  composer config --global github-oauth.github.com ${COMPOSER_GITHUB_TOKEN}
 
+[ -n "${COMPOSER_MAGENTO_USERNAME:-}" ] && \
+  composer config --global http-basic.repo.magento.com \
+    ${COMPOSER_MAGENTO_USERNAME} ${COMPOSER_MAGENTO_PASSWORD}
+
+[ -n "${COMPOSER_BITBUCKET_KEY:-}" ] && [ -n "${COMPOSER_BITBUCKET_SECRET:-}" ] && \
+  composer config --global bitbucket-oauth.bitbucket.org \
+    ${COMPOSER_BITBUCKET_KEY} ${COMPOSER_BITBUCKET_SECRET}
+
+[ -n "${COMPOSER_PRIVATE_USERNAME:-}" ] && \
+  [ -n "${COMPOSER_PRIVATE_PASSWORD:-}" ] && \
+  [ -n "${COMPOSER_PRIVATE_URL:-}" ] && \
+    composer config --global repositories.${COMPOSER_PRIVATE_URL} composer \
+      ${COMPOSER_PRIVATE_USERNAME} ${COMPOSER_PRIVATE_PASSWORD}
 
 exec "$@"
 
