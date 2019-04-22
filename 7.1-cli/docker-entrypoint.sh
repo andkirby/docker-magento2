a5872eeeb7a2b70b7cfb73c288d1ebe1ee3c7212
Andrew Roslik <andkirby@gmail.com>
2019-04-22 14:53:31 +0300

    Fixed getting incumbent user in the strict mode. Refactored code to functions using.

--- 7.1-cli/docker-entrypoint.sh
+++ 7.1-cli/docker-entrypoint.sh
@@ -10,79 +10,132 @@ set -o nounset
 # If asked, we'll ensure that the www-data is set to the same uid/gid as the
 # mounted volume.  This works around permission issues with virtualbox shared
 # folders.
-if [[ "${UPDATE_UID_GID:-}" = "true" ]]; then
-    echo "Updating www-data uid and gid"
+update_uid() {
+  echo "Updating www-data uid and gid"
 
-    DOCKER_UID=`stat -c "%u" ${MAGENTO_ROOT}`
-    DOCKER_GID=`stat -c "%g" ${MAGENTO_ROOT}`
+  local docker_uid=$(stat -c "%u" ${MAGENTO_ROOT}) \
+        docker_gid=$(stat -c "%g" ${MAGENTO_ROOT})
+  local incumbent_user=$(getent passwd ${docker_uid} | cut -d: -f1 || true) \
+        incumbent_group=$(getent group ${docker_gid} | cut -d: -f1 || true)
 
-    INCUMBENT_USER=$(getent passwd ${DOCKER_UID} | cut -d: -f1 || true)
-    INCUMBENT_GROUP=$(getent group ${DOCKER_GID} | cut -d: -f1 || true)
+  [[ -n "${incumbent_user:-}" ]] && echo "Incumbent: user = ${incumbent_user}" || true
+  [[ -n "${incumbent_group:-}" ]] && echo "Incumbent: group = ${incumbent_group}" || true
 
-    [[ -n "${INCUMBENT_USER:-}" ]] && echo "Incumbent: user = ${INCUMBENT_USER}"
-    [[ -n "${INCUMBENT_GROUP:-}" ]] && echo "Incumbent: group = ${INCUMBENT_GROUP}"
+  # Once we've established the ids and incumbent ids then we need to free them
+  # up (if necessary) and then make the change to www-data.
 
-    # Once we've established the ids and incumbent ids then we need to free them
-    # up (if necessary) and then make the change to www-data.
+  if [[ -n "${incumbent_user:-}" ]] && [[ "${incumbent_user:-}" != 'www-data' ]]; then
+      usermod -u 99${docker_uid} ${incumbent_user}
+  fi
+  if [[ -n "${incumbent_group:-}" ]] && [[ "${incumbent_group:-}" != 'www-data' ]]; then
+      groupmod -g 99${docker_gid} ${incumbent_group}
+  fi
 
-    [[ -n "${INCUMBENT_USER:-}" ]] && usermod -u 99${DOCKER_UID} ${INCUMBENT_USER}
-    usermod -u ${DOCKER_UID} www-data
+  usermod -u ${docker_uid} www-data
+  groupmod -g ${docker_gid} www-data
+}
 
-    [[ -n "${INCUMBENT_GROUP:-}" ]] && groupmod -g 99${DOCKER_GID} ${INCUMBENT_GROUP}
-    groupmod -g ${DOCKER_GID} www-data
+##
+# File system initiating
+#
+files_init() {
+  # Ensure our Magento directory exists
+  mkdir -p ${MAGENTO_ROOT}
+
+  chown www-data:www-data ${MAGENTO_ROOT}
+  chown www-data:www-data -R /var/www/.composer/cache
+}
+
+##
+# Setup cron for magento
+#
+setup_cron() {
+  CRON_LOG=/var/log/cron.log
+
+  # Setup Magento cron
+  cat <<-SHELL > /etc/cron.d/magento
+# crontab -e
+SHELL=/bin/bash
+MAILTO=''
+
+${CRON_SCHEDULE:-'* * * * *'} www-data /usr/local/bin/php ${MAGENTO_ROOT}/bin/magento cron:run | grep -v \"Ran jobs by schedule\" >> ${MAGENTO_ROOT}/var/log/magento.cron.log
+${CRON_SCHEDULE:-'* * * * *'} www-data /usr/local/bin/php ${MAGENTO_ROOT}/update/cron.php >> ${MAGENTO_ROOT}/var/log/update.cron.log
+${CRON_SCHEDULE:-'* * * * *'} www-data /usr/local/bin/php ${MAGENTO_ROOT}/bin/magento setup:cron:run >> ${MAGENTO_ROOT}/var/log/setup.cron.log
+SHELL
+
+  # Get rsyslog running for cron output
+  touch ${CRON_LOG}
+  echo "cron.* ${CRON_LOG}" > /etc/rsyslog.d/cron.conf
+  service rsyslog start
+}
+
+##
+# Setup PHP ini files
+#
+setup_php_ini() {
+  # Substitute in php.ini values
+  [[ -n "${PHP_MEMORY_LIMIT:-}" ]] \
+    && sed -i "s/memory_limit ?=.*/memory_limit = ${PHP_MEMORY_LIMIT}/" \
+          /usr/local/etc/php/conf.d/zz-magento.ini
+
+  [[ -n "${PHP_UPLOAD_MAX_FILESIZE:-}" ]] \
+    && sed -i "s/upload_max_filesize ?=.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
+          /usr/local/etc/php/conf.d/zz-magento.ini
+
+  # Enable Xdebug
+  [[ "${PHP_ENABLE_XDEBUG:-}" = "true" ]] && xd_swi on || xd_swi off
+}
+
+##
+# Setup composer keys
+#
+setup_composer() {
+  # Configure composer
+  [[ -n "${COMPOSER_GITHUB_TOKEN:-}" ]] && \
+    composer config --global github-oauth.github.com ${COMPOSER_GITHUB_TOKEN}
+
+  [[ -n "${COMPOSER_MAGENTO_USERNAME:-}" ]] && \
+    composer config --global http-basic.repo.magento.com \
+      ${COMPOSER_MAGENTO_USERNAME} ${COMPOSER_MAGENTO_PASSWORD}
+
+  [[ -n "${COMPOSER_BITBUCKET_KEY:-}" ]] && [[ -n "${COMPOSER_BITBUCKET_SECRET:-}" ]] && \
+    composer config --global bitbucket-oauth.bitbucket.org \
+      ${COMPOSER_BITBUCKET_KEY} ${COMPOSER_BITBUCKET_SECRET}
+
+  [[ -n "${COMPOSER_PRIVATE_USERNAME:-}" ]] && \
+    [[ -n "${COMPOSER_PRIVATE_PASSWORD:-}" ]] && \
+    [[ -n "${COMPOSER_PRIVATE_URL:-}" ]] && \
+      composer config --global repositories.private composer ${COMPOSER_PRIVATE_URL} && \
+      composer config --global http-basic.$(echo ${COMPOSER_PRIVATE_URL} | sed -r 's#(^https?://)([^/]+)(.*)#\2#g') \
+        ${COMPOSER_PRIVATE_USERNAME} ${COMPOSER_PRIVATE_PASSWORD}
+}
+
+##
+# Start sendmail service
+#
+setup_sendmail() {
+  /etc/init.d/sendmail start
+}
+
+################################################################################
+### Code scenario ###
+
+if [[ "${UPDATE_UID_GID:-}" = 'true' ]]; then
+  update_uid
 fi
 
-# Ensure our Magento directory exists
-mkdir -p ${MAGENTO_ROOT}
-chown www-data:www-data ${MAGENTO_ROOT}
+files_init
 
-CRON_LOG=/var/log/cron.log
-
-# Setup Magento cron
-echo "${CRON_SCHEDULE:-'* * * * *'} www-data /usr/local/bin/php ${MAGENTO_ROOT}/bin/magento cron:run | grep -v \"Ran jobs by schedule\" >> ${MAGENTO_ROOT}/var/log/magento.cron.log" > /etc/cron.d/magento
-echo "${CRON_SCHEDULE:-'* * * * *'} www-data /usr/local/bin/php ${MAGENTO_ROOT}/update/cron.php >> ${MAGENTO_ROOT}/var/log/update.cron.log" >> /etc/cron.d/magento
-echo "${CRON_SCHEDULE:-'* * * * *'} www-data /usr/local/bin/php ${MAGENTO_ROOT}/bin/magento setup:cron:run >> ${MAGENTO_ROOT}/var/log/setup.cron.log" >> /etc/cron.d/magento
-
-# Get rsyslog running for cron output
-touch $CRON_LOG
-echo "cron.* $CRON_LOG" > /etc/rsyslog.d/cron.conf
-service rsyslog start
+# Magento cron setup
+setup_cron
 
 # Configure Sendmail if required
-if [[ "${ENABLE_SENDMAIL:-}" == "true" ]]; then
-    /etc/init.d/sendmail start
+if [[ "${ENABLE_SENDMAIL:-}" == 'true' ]]; then
+    setup_sendmail
 fi
 
-# Substitute in php.ini values
-[[ -n "${PHP_MEMORY_LIMIT:-}" ]] \
-  && sed -i "s/memory_limit ?=.*/memory_limit = ${PHP_MEMORY_LIMIT}/" \
-        /usr/local/etc/php/conf.d/zz-magento.ini
-
-[[ -n "${PHP_UPLOAD_MAX_FILESIZE:-}" ]] \
-  && sed -i "s/upload_max_filesize ?=.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
-        /usr/local/etc/php/conf.d/zz-magento.ini
-
-# Enable Xdebug
-[[ "${PHP_ENABLE_XDEBUG:-}" = "true" ]] && xd_swi on || xd_swi off
-
-# Configure composer
-[[ -n "${COMPOSER_GITHUB_TOKEN:-}" ]] && \
-  composer config --global github-oauth.github.com ${COMPOSER_GITHUB_TOKEN}
-
-[[ -n "${COMPOSER_MAGENTO_USERNAME:-}" ]] && \
-  composer config --global http-basic.repo.magento.com \
-    ${COMPOSER_MAGENTO_USERNAME} ${COMPOSER_MAGENTO_PASSWORD}
-
-[[ -n "${COMPOSER_BITBUCKET_KEY:-}" ]] && [[ -n "${COMPOSER_BITBUCKET_SECRET:-}" ]] && \
-  composer config --global bitbucket-oauth.bitbucket.org \
-    ${COMPOSER_BITBUCKET_KEY} ${COMPOSER_BITBUCKET_SECRET}
-
-[[ -n "${COMPOSER_PRIVATE_USERNAME:-}" ]] && \
-  [[ -n "${COMPOSER_PRIVATE_PASSWORD:-}" ]] && \
-  [[ -n "${COMPOSER_PRIVATE_URL:-}" ]] && \
-    composer config --global repositories.private composer ${COMPOSER_PRIVATE_URL} && \
-    composer config --global http-basic.$(echo ${COMPOSER_PRIVATE_URL} | sed -r 's#(^https?://)([^/]+)(.*)#\2#g') \
-      ${COMPOSER_PRIVATE_USERNAME} ${COMPOSER_PRIVATE_PASSWORD}
+setup_php_ini
+setup_composer
 
 ${@}
 
